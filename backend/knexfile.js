9dc7e3ca59b2b543cdf67f1ca4a365ab833f8c37
Davi <davijustinocouto@gmail.com>
2023-04-20 19:58:32 -0300

    Trabalhando no backend: Tentando incluir a parte do knex para fazer migrações

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,23 +1,14 @@
 module.exports = {
-    development: {
-      client: 'sqlite3',
-      connection: {
-        filename: './dev.sqlite3'
-      }
-    },
-    staging: {
-      client: 'postgresql',
-      connection: {
-        database: 'knowledge',
-        user:     'postgres',
-        password: ''
-      },
-      pool: {
-        min: 2,
-        max: 10
-      },
-      migrations: {
-        tableName: 'knex_migrations'
-      }
-    }
+  client: 'postgresql',
+  connection: {
+    database: 'knowledge',
+    user: 'postgres',
+  },
+  pool: {
+    min: 2,
+    max: 10
+  },
+  migrations: {
+    tableName: 'knex_migrations'
+  }
 }
