3a50de4027a6b892d0df4dc57298b649b16fd939
Davi <davijustinocouto@gmail.com>
2023-04-24 17:28:47 -0300

    Knex funcionando para migração de dados, agora vamos implementar a validação de dados no backend

--- backend/api/user.js
+++ backend/api/user.js
@@ -1,6 +1,39 @@
+const bcrypt = require('bcrypt-nodejs')
+const passport = require('passport')
+
 module.exports = app => {
-    const save = (req, res) =>{
-        res.send('user save')
+    const{ existOrError, notExistsOrError,  equalsOrError} = app.api.validation
+     
+    const encryptPassword = password => {
+        const salt = bcrypt.genSaltSync(10)
+        return bcrypt.hashSync(password, salt)
+    }
+
+
+
+
+    const save = async (req, res) =>{
+        const user = {...req.body}
+        if(req.params.id) user.id = req.params.id
+
+        try{
+            existOrError(user.name, "")
+            existOrError(user.email, "")
+            existOrError(user.password, "")
+            existOrError(user.confirmPassword, "")
+            existOrError(user.password, user.confirmPassword, 
+                'Senhas não conferem'
+                )
+
+            const userFromDB = await app.db('users')
+                .where({email: user.email}).first()
+            if(!user.id){           
+            notExistsOrError(userFromDB, 'Usuário já cadastrado')
+        }
+        catch(msg) {
+            
+        }
+    }
     }
 
     return { save }
