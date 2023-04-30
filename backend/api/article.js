56fdfbf8bde9daed1f9bf3195015a70404a3da3b
Davi <davijustinocouto@gmail.com>
2023-04-30 08:34:08 -0300

    Routes and backend for api rest

--- backend/api/article.js
+++ backend/api/article.js
@@ -33,6 +33,26 @@ module.exports = (app) => {
   const get = async (req, res) => {
     const page = req.query.page || 1
     const result = await app.db('articles').count('id')
+    const count = parseInt(result.count)
+
+
+    app.db('articles')
+      .select('id', 'name', 'description')
+      .limit(limit).offset(page * limit - limit)
+      .then(articles => res.json({ data:articles, count, limit}))
+      .catch(err => res.status(500).send(err))
   }
 
+    const getById = (req, res) => {
+      app.db('articles')
+        .where({id: req.params.id})
+        .first()
+        .then(article => {
+          article.content = article.content.toString()
+          return res.json(article)
+        })
+        .catch(err => res.status(500).send(err))
+    }
+
+    return {save, remove, get, getById}
 }   
