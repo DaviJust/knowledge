71330cdb7f0f1866230094467b6c549043d03023
Davi <davijustinocouto@gmail.com>
2023-05-22 14:39:30 -0300

    Articles funcionando, o problema era do código KNEX para SQL

--- backend/api/article.js
+++ backend/api/article.js
@@ -7,7 +7,7 @@ module.exports = app => {
     const article = { ...req.body }
     if (req.params.id) article.id = req.params.id
 
-    try {
+    try { 
       existsOrError(article.name, 'Nome não informado')
       existsOrError(article.description, 'Descrição não informada')
       existsOrError(article.categoryId, 'Categoria não informada')
@@ -77,17 +77,17 @@ module.exports = app => {
     const categories = await app.db.raw(queries.categoryWithChildren, categoryId)
     const ids = categories.rows.map(c => c.id)
 
-    app.db({a: 'articles', u:'users'}
-      .select('a.id', 'a.name', 'a.description', 'a.imageUrl', {author: 'u.name'})
-      .limit(limit).offset(page * limit - limit)
-      .whereRaw('?? = ??', ['u.id', 'a.userId'])
-      .whereIn('categoryId', ids)
-      .orderBy('a.id', 'desc')
-      .then(articles => res.json(articles))
-      .catch(err => res.status(500).send(err))
-    )
+  
+    app.db({a: "articles", u: "users"})
+    .select('a.id', 'a.name', 'a.description', 'a.imageUrl', { author: 'u.name' })
+    .limit(limit).offset(page * limit - limit)
+    .whereRaw('?? = ??', ['u.id', 'a.userId'])
+    .whereIn('categoryId', ids)
+    .orderBy('a.id', 'desc')
+    .then(articles => res.json(articles))
+    .catch(err => res.status(500).send(err))
+    
   }
-
 
   return { save, remove, get, getById, getByCategory }
 }
---
+++ backend/config/passport.js
@@ -0,0 +1,5 @@
+const {authSecret} = require('../env')
+const passport = require('passport')
+const passportJwt = require('passport-jwt')
+const { Strategy, ExtractJwt} = passportJwt
+
