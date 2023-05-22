1584a8c69c8291b588a299fef155dd03db660537
Davi <davijustinocouto@gmail.com>
2023-05-22 17:25:10 -0300

    Autenticação do usuário, chave token para segurança da plataforma

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,19 @@
-const app = require("express") ()
-const consign =  require('consign')
+const app = require("express")()
+const consign = require('consign')
 const db = require('./config/db')
 
 
 app.db = db
 
 consign()
+    .include('./config/passport.js')
     .then('./config/middlewares.js')
     .then('./api/validation.js')
-    .then('./api')  
+    .then('./api')
+    .then('./schedule')
     .then('./config/routes.js')
     .into(app)
-    
 
-app.listen(3000, () =>
-console.log('Backend executando')) 
+app.listen(4000, () => {
+    console.log('Backend executando...')
+})
